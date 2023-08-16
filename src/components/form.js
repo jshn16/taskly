3df07925566798c34fda69d87fe98cde56c99cb2
jashan <jashannoor2001@gmail.com>
2023-08-16 11:05:12 -0600

    Added Delete Functionality

--- src/components/form.js
+++ src/components/form.js
@@ -1,77 +1,85 @@
-import React from 'react';
-import { useEffect, useState } from 'react';
+import React from "react";
+import { useEffect, useState } from "react";
 import { v4 as uuid } from "uuid";
 
 //using local storage to fetch data
-function todoData(){
-    const data= localStorage.getItem('Todos')
-    if(data){
-        return(JSON.parse(data))
-    }
-    else{
-        return([])
+function todoData() {
+    const data = localStorage.getItem("Todos");
+    if (data) {
+        return JSON.parse(data);
+    } else {
+        return [];
     }
 }
 
+function Form() {
+    const [todoValue, setTodoValue] = useState("");
 
-function Form(){
-
-    const[todoValue, setTodoValue]=useState("");
-
-    const[todos, setTodo]=useState(todoData())
+    const [todos, setTodo] = useState(todoData());
     // console.log(todos)
-    
 
-    function handleSubmit(event){
-        
+    function handleSubmit(event) {
         event.preventDefault();
 
-        let todoObject={
+        let todoObject = {
             id: uuid(),
-            todoName:todoValue,
-            todoCompleted:false
-        }
+            todoName: todoValue,
+            todoCompleted: false,
+        };
 
-            setTodo([...todos, todoObject])
+        setTodo([...todos, todoObject]);
 
-            
         setTodoValue(todoValue);
         // console.log(todoObject)
         // console.log(`${todoValue} Saved To localStorage`)
-        setTodoValue("")
+        setTodoValue("");
     }
 
-    //storing data in localStorage 
+    //storing data in localStorage
 
-    useEffect(()=>{
-        localStorage.setItem('Todos', JSON.stringify(todos))
-        
-    })
-    return(
-        <div className='container'>
-            <form className='taskForm'>
+    useEffect(() => {
+        localStorage.setItem("Todos", JSON.stringify(todos));
+    });
+
+
+    //handleDelete
+function handleDelete(id){
+    console.log(id)
+    //only show those todo's that has id not equall to the one clicked 
+    let removeTodo=todos.filter((todo)=>(todo.id !==id))
+    setTodo(removeTodo);
+}
+
+    return (
+        <div className="container">
+            <form className="taskForm">
                 <div>
-                    <input type='text' placeholder='Add Tasks' onChange={(event)=>{setTodoValue(event.target.value)}} value={todoValue}/>
-                    <button onClick={handleSubmit} type='submit'>Add</button>
+                    <input
+                        type="text"
+                        placeholder="Add Tasks"
+                        onChange={(event) => {
+                            setTodoValue(event.target.value);
+                        }}
+                        value={todoValue}
+                    />
+                    <button onClick={handleSubmit} type="submit">
+                        Add
+                    </button>
                 </div>
             </form>
 
-            <div className='todoList'>
-                
-                 {todos.length>0 &&(
-                    todos.map((todo,index)=>(
+            <div className="todoList">
+                {todos.length > 0 &&
+                    todos.map((todo, index) => (
                         <span key={index}>
+                            
                             <p>{todo.todoName}</p>
+                            <button onClick={()=>{handleDelete(todo.id)}}>Delete</button>
                         </span>
-                        
-                    ))
-                 )}
-
-                
-                
+                    ))}
             </div>
         </div>
-    )
+    );
 }
 
-export default Form
+export default Form;
